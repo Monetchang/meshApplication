71ca64bd8acdf186d40e9f6f591e382b8a0a0d06
changyh <>
2020-02-16 23:49:05 +0800

    update iOS framework

---
+++ ios/MeshSDK.framework/Headers/MeshSDK.h
@@ -0,0 +1,19 @@
+//
+//  MeshSDK.h
+//  MeshSDK
+//
+//  Created by wuzhengbin on 2020/2/13.
+//  Copyright © 2020 wuzhengbin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MeshSDK.
+FOUNDATION_EXPORT double MeshSDKVersionNumber;
+
+//! Project version string for MeshSDK.
+FOUNDATION_EXPORT const unsigned char MeshSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MeshSDK/PublicHeader.h>
+
+
---
+++ ios/Modules/BLEMesh/RCTBLEMesh.h
@@ -0,0 +1,19 @@
+//
+//  RCTMesh.h
+//  application
+//
+//  Created by MXCHIP on 2019/12/23.
+//  Copyright © 2012-2019 MXCHIP - Smart Plus Team. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTLog.h>
+#import <React/RCTEventEmitter.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTBLEMesh : RCTEventEmitter <RCTBridgeModule>
+
+@end
+
+NS_ASSUME_NONNULL_END
